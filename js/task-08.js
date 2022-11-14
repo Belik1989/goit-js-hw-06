e4faba2bd6fc5d121e14316938c526beb238fdb0
Bilogolovyi Sergii <sergey.hotchili@gmail.com>
2022-11-14 23:30:28 +0800

    finished 8 and 9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', fieldsCheck);
+
+function fieldsCheck(event) {
+
+    event.preventDefault();
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password =formElements.password.value;
+
+    if (email.length === 0 || password.length === 0) {
+        alert(' Fill in all forms');
+    }
+
+    const formData ={
+        email,
+        password,
+    };
+    console.log(formData);
+
+    formRef.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const body = document.body;
+const widgetEl = document.querySelector(".widget");
+const changColor =  document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+
+changeColorBtn.addEventListener("click", changeColor);
+
+function changeColor(event) {
+
+  const color = changColor.textContent;
+    changColor.textContent = getRandomHexColor();
+      body.style.backgroundColor = color;
+      // console.log(color);
+}
